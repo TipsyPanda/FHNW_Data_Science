41cbe750f69d7922a8aa3ca164093ff655e2ed9b
Yannick schmid <yannick.schmid@gmail.com>
2022-09-30 16:22:37 +0200

    Sample Setup complete

--- Scripts/Sample_Car_Exercise.R
+++ Scripts/Sample_Car_Exercise.R
@@ -1,18 +1,19 @@
 ### Preliminaries
 #install.packages("tidyr")
 #install.packages("corrplot")
+#install.packages("here")
+# install.packages("fastDummies")
 library(tidyr)
 library(corrplot) 
-# install.packages("fastDummies")
 library(fastDummies)
-#install.packages("here")
 library(here)
 
 #LOAD DATA
 dataIn = "./Data/IN/cgh_cars.csv"
 dataOut = "./Data/Out/my_cars.csv"
-my_cars <- read.csv(dataIn) 
-
+cars_in <- read.csv(dataIn) 
+View(cars_in)
+my_cars <- cars_in
 
 #drop ship
 my_cars <- my_cars[my_cars$type != 'ship',]
@@ -22,17 +23,32 @@ my_cars <- dummy_cols(my_cars, select_columns="type", remove_selected_columns = 
 my_cars <- dummy_cols(my_cars, select_columns="brand", remove_selected_columns = TRUE)
 #ordered style
 my_cars$style <- as.integer(ordered(my_cars$style, levels= c("basic", "medium", "luxus")))
+
+#Handle 0 cases
+my_cars$mpg <- ifelse(my_cars$mpg == 0.0, NA, my_cars$mpg) 
 #Handle NA cases
+my_cars$mpg <- ifelse(is.na(my_cars$mpg),
+                       mean(my_cars$mpg, na.rm=TRUE),
+                      my_cars$mpg)
+
 
 #Replace negative values in cyl
 my_cars$cyl <- ifelse(my_cars$cyl <0 , abs(my_cars$cyl), my_cars$cyl)
 
 #round gears
-my_cars$gear<- round(my_cars, digits = 0)
+my_cars$gear<- round(my_cars$gear, digits = 0)
 
 #remove weight outlines
+while ( colSums(my_cars["wt"] %/% 10 > 0 ) > 0 ) 
+{
+  my_cars$wt <- ifelse(my_cars$wt > 10, my_cars$wt /10 ,my_cars$wt)
+}
+
+#Remove empty price rows
+my_cars <- drop_na(my_cars,price)
 
 
 #View results
+corrplot(cor(my_cars), type = "upper", order = "hclust", tl.col = "black", tl.srt = 45)
 write.csv(my_cars,dataOut) 
 View(my_cars)
