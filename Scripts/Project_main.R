5d2aec3de9e7e4d42b2c61987268ddc3a922d311
Yannick schmid <yannick.schmid@gmail.com>
2022-10-06 09:59:42 +0200

    Sample column 6 term

--- Scripts/Project_main.R
+++ Scripts/Project_main.R
@@ -3,10 +3,13 @@
 #install.packages("corrplot")
 #install.packages("here")
 # install.packages("fastDummies")
+# install.packages("tm")
 library(tidyr)
 library(corrplot) 
 library(fastDummies)
 library(here)
+library(tm)
+
 
 #LOAD DATA
 dataIn = "./Data/IN/Project/LCdata.csv"
@@ -14,10 +17,20 @@ dataOut = "./Data/Out/Project/LCdata_out.csv"
 Lcdata_in <- read.csv(dataIn,header = TRUE, sep =';') 
 lcdata <- Lcdata_in
 
+
+
 summary(lcdata)
+
+#C6.Term 
+f <- factor(lcdata$term)
+levels(f)
+require("tm")
+lcdata$term <- as.integer(removeWords(lcdata$term,"months"))
+
+
 
 
 #View results
-write.csv(lcdata,dataOut) 
+#write.csv(lcdata,dataOut) 
 
 View(lcdata)
